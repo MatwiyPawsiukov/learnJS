4d249bf8c6f0658dd505807f01921b8475de52c4
Matwi2343 <centuriongg2343@gmail.com>
2025-08-04 16:53:39 +0200

    h

---
+++ datatTransform.js
@@ -0,0 +1,52 @@
+/* parseInt() */
+
+
+
+/*let number1 = "56hello"
+let number2 = 4
+
+ console.log(parseInt(number1) + number2);
+ */
+
+ /*let number1 = "6h8j1h66h7f1c4"
+    let number2 = parseInt(number1)
+
+     console.log(number2)
+     */
+
+
+
+/*console.log(parseInt("abcd"));
+
+const type = typeof NaN
+
+console.log(type);*/
+
+/* isNaN
+
+const num1 = "JavaScript"
+const num2  = "58"
+let result = isNaN(num1)
+
+console.log(result);
+
+result = isNaN(num2)
+
+console.log(result);
+*/
+
+/*parseFloat()
+const num1 = "42.31"
+const num2 = "7.21"
+let result = parseFloat(num1) + parseFloat(num2) 
+
+console.log(result);
+*/
+
+
+
+
+
+
+    
+ 
