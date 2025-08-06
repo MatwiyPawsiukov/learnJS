0a8660399a10ce7a5d474e40046a85ddb5d84119
Matwi2343 <centuriongg2343@gmail.com>
2025-08-06 18:24:06 +0200

    changes

---
+++ cycles.js
@@ -0,0 +1,42 @@
+/*for(let i = 0; i < 100; i+=2){
+    console.log(i)
+}
+*/
+
+
+/*for(let i = 0; i < 60;){
+    console.log(i);
+    i = i + 10
+}
+ */
+
+
+/*const people = ["Matwiy",  "Sergiy", "Stas"]
+
+for(let i = 0; i < 3; i++){
+
+    console.log(people[i]);
+}
+*/
+
+
+/*function itt(){
+for(let i = 1, j = 1; i < 5, j < 4; i++, j++){
+    console.log(i + j);
+    }
+}
+itt()
+*/
+
+/*for(let i = 0; i < 5; console.log(i++))
+    console.log("end");
+*/
+
+/*let i = 0
+
+for(console.log("init"); i < 5; i++){
+    console.log(i);
+}
+*/
+
+
---
+++ functions.js
@@ -0,0 +1,64 @@
+/*let matwiy = ["Matwiy", 14, "matwiy@gmail.com"]
+let sergiy = ["Sergiy", 21, "sergiy@gmail.com"]
+
+function printPerson(username, age, email){
+    console.log("Name:", username)
+    console.log("Age:", age)
+    console.log("Email:", email)
+    console.log("=======================")
+}
+
+printPerson(...matwiy)
+printPerson(...sergiy)
+*/
+
+/*function sum(x = 8, y = 10) {
+  const u = x + y;
+  console.log(z);
+}
+
+sum()
+sum(2)
+sum(9 , 1)
+*/
+
+/*function sum(x = 8, y = 10 + x){
+    const u  = x + y
+    console.log(u)
+}
+sum()
+sum(6)
+*/
+
+/*function sum(a, b, c){
+    console.log("a =", a);
+    console.log("b =", b);
+    console.log("c =", c);
+ 
+    let result = 0;
+    for(const n of arguments) 
+        result += n;
+    console.log("result =", result);
+}
+sum(6, 4, 5, 8)     // 23
+*/
+
+/*function sum(nums){
+  let result = 0
+
+  if(arguments.length >= 1){
+    result = result + arguments[0]
+  }
+  
+  if(arguments.length >= 2){
+    result = result + arguments[1]
+  }
+
+  console.log("result =", result)
+}
+
+sum(6)
+sum(6, 5)
+sum(6,5,4)
+*/
+
