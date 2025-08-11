a1cd2e0415306dd7766caf3512a0464409ae8da6
Matwi2343 <centuriongg2343@gmail.com>
2025-08-11 23:37:02 +0200

    cha

--- learning/objekts.js
+++ learning/objekts.js
@@ -12,7 +12,7 @@ console.log(user.age, user.name);
 /*const user = {} - Але я би так не робив 
 user.name = "matwiy"
 user.age = 14
-*/  
+*/
 
 /*const name = "Matwiy"
 const age = 14
@@ -30,14 +30,70 @@ console.log(teacher.user);
 console.log(teacher.course);
 */
 
-const user = {
+/*const user = {
     name: "Matwiy",
     age: 14, 
-    display: function(){
+    display(){
         console.log(this.name, this.age);
+        
+    },
+    move(place){
+        console.log(this.name, "goes to", place);
         
     }
 }
 
 
 user.display()
+user.move("the shop")
+*/
+
+/*const user = {} // то все синтаксиси я в а#!ї
+user["name"] = "Matwiy"
+user["age"] = 14
+user["display"] = function(){
+    console.log(user.name, user.age);
+    
+}
+
+user["display"]()
+*/
+
+/*const user = { // ACUH SHCEIß SYNTAKSIS
+    ["name"]: "Matwiy",
+    ["age"]:  14,
+    ["display"]:  function(){
+        console.log(user.name, user.age);
+    }
+}
+
+user["display"]()
+*/
+
+/*const user = { // auch
+    "name": "Matwiy",
+    "age": 14,
+    "display": function(){
+        console.log(user.name, user.age);
+        
+    }
+}
+user.display()
+*/
+
+/*const user = {
+  // Das brauche ich auf jeden fall noch!!!!!!!!!!!!!!!!!!!!!
+  name: "Matiwy",
+  age: 14,
+  "full name": "Matiwy Pawsiukow",
+
+  "display info": function () {
+    console.log(user.name, user.age);
+  },
+};
+
+console.log(user["full name"]);
+user["display info"]();
+*/
+
+
